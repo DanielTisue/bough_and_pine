c442831572ca8df34d03eea7f99eac6bfde426b2
DanielTisue <d.tisue@gmail.com>
2021-10-11 10:31:33 -0500

    Added routing with swtich and error page component.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 // import logo from './logo.svg';
 import './App.css';
-import { Route, Switch, Link } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
+import Navbar from './components/Navbar';
 import Home from './pages/Home';
 import RoomDesigns from './pages/RoomDesigns';
 import SingleRoom from './pages/SingleRoom';
@@ -12,6 +13,7 @@ import Error from './pages/Error';
 function App() {
   return (
     <>
+    <Navbar />
     <Switch>
     <Route exact path='/' component={Home} />
     <Route exact path='/rooms/' component={RoomDesigns} />
