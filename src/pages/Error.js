66bb978a9d23448879b934d7dd55b7f3dc754377
DanielTisue <d.tisue@gmail.com>
2021-10-14 21:31:01 -0500

    Added banner component to error page.

--- src/pages/Error.js
+++ src/pages/Error.js
@@ -1,9 +1,18 @@
+import Hero from '../components/Hero';
+import Banner from '../components/Banner';
+import { Link } from 'react-router-dom';
 
 function Error() {
   return (
-   <div className="container">
-      Error Page
+  <Hero hero="landingHero">
+   <div className="container container-center">
+     <Banner title="404" subtitle="Error" text="This page does not exist">
+        <Link to="/">
+          <p className="p2">Let's go back home</p>
+        </Link>
+    </Banner>
     </div>
+  </Hero>
   )
 }
 
