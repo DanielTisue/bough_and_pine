82c8824fd6c168ddd45e7382ca2aa309ea1f7eaa
DanielTisue <d.tisue@gmail.com>
2021-10-14 21:31:52 -0500

    Added additonal test text with global styles.

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,33 @@
-
+import Hero from '../components/Hero';
 
 export default function Home() {
   return (
     <>
-      {/* <div className="defaultHero"></div> */}
-      <div className="container">Home Page</div>
+      <Hero hero="landingHero">
+        <div className="container">
+          <div className="banner-text-wrapper">
+            <h1>Home Page</h1>
+            
+          </div>
+        </div>
+      </Hero>
+      <div className="container-center">
+        <h1>More stuff</h1>
+        <p className="p1">class p1</p>
+        <p className="p2">class p2</p>
+      </div>
+      <div className="container-center">
+        <h2>More stuff</h2>
+        <p className="p1">class p1</p>
+        <p className="p2">class p2</p>
+      </div>
+      <div className="container-center">
+        <h3>More stuff</h3>
+        <p className="p1">class p1</p>
+        <p className="p2">class p2</p>
+      </div>
+     
+      
     </>
   )
 }
