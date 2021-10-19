2302c85c8246a92dcd7c52ab921fb07aae49fbd2
DanielTisue <d.tisue@gmail.com>
2021-10-18 19:17:40 -0500

    Re-factored component to accomodate title, subtitle, and text layout.

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -2,9 +2,11 @@
 function Banner({ title, subtitle, text, children }) {
   return (
     <div className="banner-text-wrapper">
-      <h1>{title}</h1>
-      <h2>{subtitle}</h2>
-      <p className="p2">{children}</p>
+      <h1 className="title">{title}</h1>
+      <h2 className="subtitle">{subtitle}</h2>
+      <div className="text-container">
+      <p className="p2 section-text center-align">{text}</p>
+      </div>
     </div>
   )
 }
