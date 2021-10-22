239fdb8760ce273e7534fc747b688bb186b3635b
DanielTisue <d.tisue@gmail.com>
2021-10-21 21:23:24 -0500

    Revised layout.

--- src/pages/Error.js
+++ src/pages/Error.js
@@ -1,6 +1,6 @@
-import Hero from '../components/Hero';
+// import Hero from '../components/Hero';
 import Banner from '../components/Banner';
-import { Link } from 'react-router-dom';
+// import { Link } from 'react-router-dom';
 
 function Error() {
   return (
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,37 +1,26 @@
 import Banner from '../components/Banner';
 import Hero from '../components/Hero';
+import Services from '../components/Services';
 
 export default function Home() {
   return (
     <>
       <Hero hero="landingHero">
-        <div className="container">
-          <Banner title="Luxury Home" subtitle="Decor" text="TLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book." />
+        <div className="container-center">
+          <Banner title="Luxury Home" subtitle="Decor" text="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book." />
         </div>
       </Hero>
       <div className="container-center">
-        <p className="p2 center-align">Menu</p>
-        <h1>More Stuff</h1>
-        <h2>Branding</h2>
-        <p className="p1">class p1</p>
-
-        <div className="text-container">
-          <p className="p2">class p2: class p2: Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
-        </div>
+          <Banner title="Artistic Touch" subtitle="Work" text="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book." />
       </div>
       <div className="container-center">
-        <h1>More Stuff</h1>
-        <h2>Branding</h2>
-        <div className="text-container">
-          <p className="p2">class p2: Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
-        </div>
-        <p className="p2">class p2</p>
+          <Banner title="Design Practice" subtitle="Technique" text="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book." />
       </div>
+     
       <div className="container-center">
-        <h3>More stuff</h3>
-        <p className="p1">class p1</p>
-        <p className="p2">class p2</p>
+       <Services />
       </div>
+     
      
       
     </>
