14e64da40c288d0d6e15367ec366a1d2aa5819ff
DanielTisue <d.tisue@gmail.com>
2021-11-07 19:09:54 -0600

    Fixed import statement for roomprovider.

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './App.css';
 import App from './App';
 import { BrowserRouter as Router } from 'react-router-dom';
-import { RoomProvider } from "./Context";
+import { RoomProvider } from "./hooks/Context";
 
 ReactDOM.render(
   <RoomProvider>
