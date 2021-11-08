1a4c96cb14f37f11dffbce97ba531791445864d4
DanielTisue <d.tisue@gmail.com>
2021-11-07 19:09:08 -0600

    Added mobile responsiveness and tested hook.

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,7 @@
 import Banner from '../components/Banner';
 import Hero from '../components/Hero';
 import Services from '../components/Services';
+import FeaturedRooms from '../components/FeaturedRooms';
 
 export default function Home() {
   return (
@@ -13,6 +14,7 @@ export default function Home() {
       <div className="container-center">
         <section className="work">
           <Banner title="Artistic Touch" subtitle="Work" text="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book." />
+          <FeaturedRooms />
         </section>
         <section className="services">
           <Banner title="What we offer" subtitle="Services" />
