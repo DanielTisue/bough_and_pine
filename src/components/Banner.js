7177b49dc080d7d399c456562314d92c825a392c
DanielTisue <d.tisue@gmail.com>
2021-11-15 07:46:58 -0600

    Refactored banner.js styling with grid. Tested examples om homepoage.

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,12 +1,11 @@
 
-function Banner({ title, subtitle, text, children }) {
+function Banner({ title1, title2, subtitle, text, children }) {
   return (
     <div className="banner-text-wrapper">
-      <h1 className="title">{title}</h1>
+      <h1 className="title-one">{title1}</h1>
+      <h1 className="title-two">{title2}</h1>
       <h2 className="subtitle">{subtitle}</h2>
-      <div className="text-container">
-      <p className="p2 section-text center-align">{text}</p>
-      </div>
+      <p className="banner-text">{text}</p>
     </div>
   )
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,7 @@
 import Banner from '../components/Banner';
 import Hero from '../components/Hero';
 import Services from '../components/Services';
+import mainImage from '../images/livingimg3.jpg'
 import FeaturedRooms from '../components/FeaturedRooms';
 
 export default function Home() {
@@ -13,8 +14,12 @@ export default function Home() {
       </Hero>
       <div className="container-center">
         <section className="work">
-          <Banner title="Artistic Touch" subtitle="Work" text="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book." />
-          <FeaturedRooms />
+          <div className="left-section">
+          <Banner title1="Artistic" title2="Touch" subtitle="Work" text="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum is simply dummy text of the printing and typesetting industry." />
+          </div>
+          <div className="right-section">
+          <img className="main-image" src={mainImage} />
+          </div>
         </section>
         <section className="services">
           <Banner title="What we offer" subtitle="Services" />
