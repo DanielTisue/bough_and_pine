a51c6301901e660ea1e5705ec97aa0e586f6a043
DanielTisue <d.tisue@gmail.com>
2021-11-16 10:47:23 -0600

    Created Title component.

---
+++ src/components/Title.js
@@ -0,0 +1,11 @@
+function Title ({ title, subtitle, text, children }) {
+  return (
+    <div className="title-text-wrapper">
+      <h1 className="title">{title}</h1>
+      <h2 className="subtitle">{subtitle}</h2>
+      <p className="title-text">{text}</p>
+    </div>
+  )
+}
+
+export default Title;
