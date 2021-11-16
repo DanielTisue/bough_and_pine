aedc71e3e870fa6fb54ee9e32aeae896e76b1a02
DanielTisue <d.tisue@gmail.com>
2021-11-16 10:49:46 -0600

    added new banner component with props and title component.

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,8 @@
 import Banner from '../components/Banner';
 import Hero from '../components/Hero';
 import Services from '../components/Services';
-import mainImage from '../images/livingimg3.jpg'
+import mainImage from '../images/livingimg3.jpg';
+import Title from '../components/Title';
 import FeaturedRooms from '../components/FeaturedRooms';
 
 export default function Home() {
@@ -15,21 +16,29 @@ export default function Home() {
       <div className="container-center">
         <section className="work">
           <div className="left-section">
-          <Banner title1="Artistic" title2="Touch" subtitle="Work" text="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum is simply dummy text of the printing and typesetting industry." />
+          <Banner title1="Artistic" title2="Touch" bannerSubtitle="Work" bannerText="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum is simply dummy text of the printing and typesetting industry." />
           </div>
           <div className="right-section">
           <img className="main-image" src={mainImage} />
           </div>
         </section>
         <section className="services">
-          <Banner title="What we offer" subtitle="Services" />
+          <Title title="What we offer" subtitle="Services" />
           <Services />
         </section>
         <section className="process">
-          <Banner title="Design Practice" subtitle="Technique" />
+          <Title title="Design Practice" subtitle="Technique" />
+          
+          {/* Have too change left/right width and image padding/margin */}
+          {/* <div className="left-section">
+          <img className="main-image" src={mainImage} />
+          </div>
+           <div className="right-section">
+          <Banner title1="Design" title2="Practice" bannerSubtitle="Technique" bannerText="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum is simply dummy text of the printing and typesetting industry." />
+          </div> */}
         </section>
         <section className="contact">
-          <Banner title="Let's work together" subtitle="Collaborate" />
+          <Title title="Let's work together" subtitle="Collaborate" />
         </section>
       </div>
     </>
