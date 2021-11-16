8698987513d5f3cc63307f34eac0f97776aee5cd
DanielTisue <d.tisue@gmail.com>
2021-11-16 10:48:24 -0600

    Renamed elements in banner component and adjusted styles for both banner and title component.

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,11 +1,11 @@
 
-function Banner({ title1, title2, subtitle, text, children }) {
+function Banner({ title1, title2, bannerSubtitle, bannerText, children }) {
   return (
     <div className="banner-text-wrapper">
       <h1 className="title-one">{title1}</h1>
       <h1 className="title-two">{title2}</h1>
-      <h2 className="subtitle">{subtitle}</h2>
-      <p className="banner-text">{text}</p>
+      <h2 className="banner-subtitle">{bannerSubtitle}</h2>
+      <p className="banner-text">{bannerText}</p>
     </div>
   )
 }
