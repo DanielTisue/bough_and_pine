b08420bb159f32e1adb5a3fe7e74c0ce841d47b0
DanielTisue <d.tisue@gmail.com>
2021-12-12 21:17:04 -0600

    Added id to title div for specific cta banner styling.

--- src/components/Title.js
+++ src/components/Title.js
@@ -1,6 +1,6 @@
 function Title ({ title, subtitle, text, children }) {
   return (
-    <div className="title-text-wrapper">
+    <div id="cta-text-wrapper" className="title-text-wrapper">
       <h1 className="title">{title}</h1>
       <h2 className="subtitle">{subtitle}</h2>
       <p className="title-text">{text}</p>
